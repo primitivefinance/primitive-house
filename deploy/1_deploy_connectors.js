3223678a6e1af478d6112a6257bd2006412d256b
Alexangelj <alexangel.j@gmail.com>
2021-01-20 21:38:46 -0800

    Adds blank slate using current state of v1-connectors repo.

---
+++ deploy/1_deploy_connectors.js
@@ -0,0 +1,24 @@
+const bre = require('hardhat')
+
+const UNISWAPV2_ROUTER02 = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+const UNISWAPV2_FACTORY = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { log, deploy } = deployments
+  const { deployer } = await getNamedAccounts()
+  const chain = await bre.getChainId()
+
+  const uniswapConnector = await deploy('UniswapConnector03', {
+    from: deployer,
+    contractName: 'UniswapConnector03',
+    args: [UNISWAPV2_ROUTER02, UNISWAPV2_FACTORY, TRADER[chain].address],
+  })
+
+  let deployed = [uniswapConnector]
+  for (let i = 0; i < deployed.length; i++) {
+    if (deployed[i].newlyDeployed)
+      log(`Contract deployed at ${deployed[i].address} using ${deployed[i].receipt.gasUsed} gas on chain ${chain}`)
+  }
+}
+
+module.exports.tags = ['Periphery']
