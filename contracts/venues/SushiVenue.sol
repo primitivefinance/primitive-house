27a3f60ddac2cbfcff10e511e56b3f27c188c595
Kirk Hutchison <onetruekirk@protonmail.com>
2021-03-06 14:34:43 -0800

    Add sushi venue and test file

---
+++ contracts/venues/SushiVenue.sol
@@ -0,0 +1,249 @@
+pragma solidity ^0.7.1;
+
+/**
+ * @title Utilized for ongoing development.
+ */
+
+// Open Zeppelin
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+// Uniswap
+import {
+    IUniswapV2Pair
+} from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import {
+    IUniswapV2Router02
+} from "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import {
+    IUniswapV2Factory
+} from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+
+// Internal
+import {VaultVenue} from "./VaultVenue.sol";
+import {SafeMath} from "../libraries/SafeMath.sol";
+
+import "hardhat/console.sol";
+
+contract SushiVenue is VaultVenue {
+    using SafeERC20 for IERC20; // Reverts when `transfer` or `transferFrom` erc20 calls don't return proper data
+    using SafeMath for uint256; // Reverts on math underflows/overflows
+
+    IUniswapV2Factory public factory =
+        IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f); // The Uniswap V2 factory contract to get pair addresses from
+    IUniswapV2Router02 public router =
+        IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D); // The Uniswap contract used to interact with the protocol
+
+    // ===== Constructor =====
+
+    constructor(
+        address weth_,
+        address house_,
+        address wToken_,
+        address factory_,
+        address router_
+    ) VaultVenue(weth_, house_, wToken_) {
+      factory = IUniswapV2Factory(factory_);
+      router = IUniswapV2Router02(router_);
+    }
+
+    // ===== Mutable =====
+
+    /**
+     * @notice A basic function to deposit an option into a wrap token, and return it to the _house.
+     */
+    function mintOptionsThenWrap(
+        bytes32 oid,
+        uint256 amount,
+        address receiver
+    ) public returns (bool) {
+        // Receivers are this address
+        address[] memory receivers = new address[](2);
+        receivers[0] = address(this);
+        receivers[1] = address(this);
+
+        console.log("minting options");
+        // Call the _house to mint options, pulls base tokens to the _house from the executing caller
+        _mintOptions(oid, amount, receivers, false);
+        // Get option addresses
+        (address long, address short) = _house.getOptionTokens(oid);
+        // Send option tokens to a wrapped token, which is then sent back to the _house.
+        _wrapOptionForHouse(oid, amount);
+        return true;
+    }
+// let a venue borrow options. TEST FUNCTION ONLY DO NOT DEPLOY
+// todo make specific VenueTest.sol with test utility fns
+    function borrowOptionTest(
+      bytes32 oid,
+      uint256 amount
+    ) public returns (bool) {
+      // Receivers are this address
+      address[] memory receivers = new address[](2);
+      receivers[0] = address(this);
+      receivers[1] = address(this);
+
+      _house.borrowOptions(oid, amount, receivers);
+      return true;
+    }
+
+    /**
+     * @notice  Exercises the `oid` option using the balance of the long stored in this contract.
+     */
+    function exerciseFromBalance(
+        bytes32 oid,
+        uint256 amount,
+        address receiver
+    ) public {
+        (address long, ) = _house.getOptionTokens(oid);
+        uint256 balance = balanceOfUser[_house.getExecutingCaller()][long];
+        console.log("checking balance in venue", balance, amount);
+        require(balance >= amount, "Venue: ABOVE_MAX");
+        // exercise options using the House
+        console.log("venue._exerciseOptions");
+        _exerciseOptions(oid, amount, receiver, true);
+    }
+
+    function exerciseFromWrappedBalance(
+        bytes32 oid,
+        uint256 amount,
+        address receiver
+    ) public {
+        // Receivers are this address
+        address[] memory receivers = new address[](2);
+        receivers[0] = address(this);
+        receivers[1] = address(this);
+        splitOptionsAndDeposit(oid, amount, receivers);
+        (address long, ) = _house.getOptionTokens(oid);
+        uint256 balance = balanceOfUser[_house.getExecutingCaller()][long];
+        console.log("checking balance in venue", balance, amount);
+        require(balance >= amount, "Venue: ABOVE_MAX");
+        // exercise options using the House
+        console.log("venue._exerciseOptions");
+        _exerciseOptions(oid, amount, receiver, true);
+    }
+
+    function redeemFromBalance(
+        bytes32 oid,
+        uint256 amount,
+        address receiver
+    ) public {
+        (, address short) = _house.getOptionTokens(oid);
+        uint256 balance = balanceOfUser[_house.getExecutingCaller()][short];
+        console.log("checking balance in venue", balance, amount);
+        require(balance >= amount, "Venue: ABOVE_MAX");
+        // exercise options using the House
+        console.log("venue._exerciseOptions");
+        _redeemOptions(oid, amount, receiver, true);
+    }
+
+    function closeFromWrappedBalance(
+        bytes32 oid,
+        uint256 amount,
+        address receiver
+    ) public {
+        // Receivers are this address
+        address[] memory receivers = new address[](2);
+        receivers[0] = address(this);
+        receivers[1] = address(this);
+        splitOptionsAndDeposit(oid, amount, receivers);
+
+        (address long, ) = _house.getOptionTokens(oid);
+        uint256 balance = balanceOfUser[_house.getExecutingCaller()][long];
+        console.log("checking balance in venue", balance, amount);
+        require(balance >= amount, "Venue: ABOVE_MAX");
+        // exercise options using the House
+        console.log("venue._exerciseOptions");
+        _closeOptions(oid, amount, receiver, true);
+    }
+
+    function closeFromBalance(
+        bytes32 oid,
+        uint256 amount,
+        address receiver
+    ) public {
+        (address long, ) = _house.getOptionTokens(oid);
+        uint256 balance = balanceOfUser[_house.getExecutingCaller()][long];
+        console.log("checking balance in venue", balance, amount);
+        require(balance >= amount, "Venue: ABOVE_MAX");
+        // exercise options using the House
+        console.log("venue._closeOptions");
+        _closeOptions(oid, amount, receiver, true);
+    }
+
+    function close(
+        bytes32 oid,
+        uint256 amount,
+        address receiver
+    ) public {
+        (address long, ) = _house.getOptionTokens(oid);
+        uint256 balance = balanceOfUser[_house.getExecutingCaller()][long];
+        console.log("checking balance in venue", balance, amount);
+        require(balance >= amount, "Venue: ABOVE_MAX");
+        // exercise options using the House
+        console.log("venue._closeOptions");
+        _closeOptions(oid, amount, receiver, false);
+    }
+
+    // ===== Flash Loans =====
+
+    function _flashSwap(
+        IUniswapV2Pair pair,
+        address token,
+        uint256 amount,
+        bytes memory params
+    ) internal returns (bool) {
+        // Receives `amount` of `token` to this contract address.
+        uint256 amount0Out = pair.token0() == token ? amount : 0;
+        uint256 amount1Out = pair.token0() == token ? 0 : amount;
+        // Execute the callback function in params.
+        pair.swap(amount0Out, amount1Out, address(this), params);
+        return true;
+    }
+
+    /**
+      * @dev     The callback function triggered in a UniswapV2Pair.swap() call when the `data` parameter has data.
+      * @param   sender The original msg.sender of the UniswapV2Pair.swap() call.
+      * @param   amount0 The quantity of token0 received to the `to` address in the swap() call.
+      * @param   amount1 The quantity of token1 received to the `to` address in the swap() call.
+      * @param   data The payload passed in the `data` parameter of the swap() call.
+      */
+      function uniswapV2Call(
+        address sender,
+        uint256 amount0,
+        uint256 amount1,
+        bytes calldata data
+      ) external {
+            require(
+              msg.sender ==
+              factory.getPair(
+                  IUniswapV2Pair(msg.sender).token0(),
+                  IUniswapV2Pair(msg.sender).token1()
+              )
+            ); // ensure that msg.sender is actually a V2 pair
+            require(sender == address(this), "NOT_SENDER"); // ensure called by this contract
+            (bool success, bytes memory returnData) = address(this).call(data);
+            require(
+                success &&
+                (returnData.length == 0 || abi.decode(returnData, (bool))),
+                "CALLBACK"
+            );
+      }
+
+    // ===== Test =====
+
+    uint256 private _test;
+
+    function getTest() public view returns (uint256) {
+        return _test;
+    }
+
+    event SetTest(uint256 value);
+
+    function setTest(uint256 value) public {
+        _test = value;
+
+        emit SetTest(value);
+    }
+
+    // ===== View =====
+}
---
+++ test/SushiVenue.test.ts
@@ -0,0 +1,365 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address'
+import chai, { expect } from 'chai'
+import { solidity } from 'ethereum-waffle'
+chai.use(solidity)
+import { BigNumber, BigNumberish, Contract } from 'ethers'
+import { parseEther, formatEther } from 'ethers/lib/utils'
+import { ethers, waffle } from 'hardhat'
+import { deploy } from '../scripts/deploy'
+import { deployTokens, deployWeth, batchApproval, tokenFromAddress } from './lib/erc20'
+import { deployMultiToken } from './lib/MultiToken'
+import {} from './lib/protocol'
+import { log } from './lib/utils'
+import generateReport from './lib/table/generateReport'
+const { AddressZero } = ethers.constants
+
+import UniswapV2Router02 from '@uniswap/v2-periphery/build/UniswapV2Router02.json'
+import UniswapV2Factory from '@uniswap/v2-core/build/UniswapV2Factory.json'
+
+describe("House integration tests using sushi venue", function () {
+
+  let signers: SignerWithAddress[]
+  let weth: Contract
+  let house: Contract
+  let signer: SignerWithAddress
+  let Alice: string
+  let tokens: Contract[], comp: Contract, dai: Contract, MultiToken: Contract
+  let venue: Contract
+  let manager: Contract
+  let core: Contract
+  let baseToken, quoteToken, strikePrice, expiry, isCall
+  let oid: string
+  let false_oid: string
+  let longToken: Contract, shortToken: Contract
+  let uniswapFactory: Contract, uniswapRouter: Contract
+
+  const deadline = Math.floor(Date.now() / 1000) + 60 * 20
+
+  const getTokenAddresses = () => {
+    const array = tokens.map((token) => {
+      return token.address
+    })
+    return array
+  }
+
+  const getAmounts = () => {
+    let ONE: BigNumber = parseEther('1')
+    const array = tokens.map(() => {
+      return ONE
+    })
+    return array
+  }
+
+  const setTest = (venue: Contract, signer: string) => {
+    let quantity: BigNumber = parseEther('1')
+    let params: any = venue.interface.encodeFunctionData('setTest', [quantity])
+    return house.execute(0, venue.address, params)
+  }
+
+  const venueDeposit = (venue: Contract, receiver: string, oid: string) => {
+    let amount: BigNumber = parseEther('1')
+    let params: any = venue.interface.encodeFunctionData('deposit', [oid, amount, receiver])
+    return house.execute(0, venue.address, params)
+  }
+
+  const venueMintWrap = (venue: Contract, receiver: string, oid: string) => {
+    let amount: BigNumber = parseEther('1')
+    let params: any = venue.interface.encodeFunctionData('mintOptionsThenWrap', [oid, amount, receiver])
+    return house.execute(0, venue.address, params)
+  }
+// todo, util function to simply mint options for user on demand
+  const venueSplitDeposit = (venue: Contract, receiver: string, oid: string) => {
+    let amount: BigNumber = parseEther('1')
+    let params: any = venue.interface.encodeFunctionData('splitOptionsAndDeposit', [oid, amount, [receiver, receiver]])
+    return house.execute(1, venue.address, params)
+  }
+
+  const quoteTokenDeposit = (venue: Contract, receiver: string, quoteToken: Contract) => {
+    let amount: BigNumber = parseEther('2')
+    // deposit quote tokens to balance
+    let depositParams: any = venue.interface.encodeFunctionData('depositToken', [
+      quoteToken.address,
+      amount.mul(strikePrice).div(parseEther('1')),
+      receiver,
+    ])
+    return house.execute(1, venue.address, depositParams)
+  }
+
+  const venueExercise = (venue: Contract, receiver: string, oid: string, quoteToken: Contract) => {
+    let amount: BigNumber = parseEther('2')
+
+    let params: any = venue.interface.encodeFunctionData('exerciseFromBalance', [oid, amount, receiver])
+    return house.execute(1, venue.address, params)
+  }
+
+  beforeEach(async function() {
+    // 1. get signers
+    signers = await ethers.getSigners()
+    signer = signers[0]
+    Alice = signer.address
+
+    // 2. get weth, erc-20 tokens, and wrapped tokens
+    weth = await deployWeth(signer)
+    tokens = await deployTokens(signer, 2, ['comp', 'dai'])
+    ;[comp, dai] = tokens
+    MultiToken = await deployMultiToken(signer)
+
+    // Deploy uniswap instances
+    uniswapFactory = await waffle.deployContract(signers[0], UniswapV2Factory, [signers[0].address], {gasLimit: 9500000})
+    uniswapRouter = await waffle.deployContract(signers[0], UniswapV2Router02, [uniswapFactory.address, weth.address], {
+      gasLimit: 9500000,
+    })
+
+    // 3. select option params
+    baseToken = comp
+    quoteToken = dai
+    strikePrice = parseEther('1000')
+    expiry = 1615190111
+    isCall = true
+
+    // 4. deploy house
+    house = await deploy('House', { from: signers[0], args: [AddressZero] })
+
+    // 5. deploy venue
+    venue = await deploy('SushiVenue', { from: signers[0], args: [weth.address, house.address, MultiToken.address, uniswapFactory.address, uniswapRouter.address] })
+
+    // 6. deploy core with the house as the manager
+    core = await deploy('Core', { from: signers[0], args: [house.address] })
+
+    // 7. create options
+    await core.createOption(baseToken.address, quoteToken.address, strikePrice, expiry, isCall)
+
+    // 8. get the oid for the created options
+    oid = await core.getOIdFromParameters(baseToken.address, quoteToken.address, strikePrice, expiry, isCall)
+    false_oid = await core.getOIdFromParameters(Alice, Alice, strikePrice, expiry, isCall)
+
+
+    // 9. get the tokens for the oid
+    let [longAddr, shortAddr] = await core.getTokenData(oid)
+
+    // 10. get erc20 instances for the tokenization so we can query balances
+    longToken = tokenFromAddress(longAddr, signers[0])
+    shortToken = tokenFromAddress(shortAddr, signers[0])
+
+    // 11. set the core in the house
+    await house.setCore(core.address)
+
+    let contractNames: string[] = ['House']
+    let contracts: Contract[] = [house]
+    let addresses: string[] = [signer.address]
+    let addressNamesArray: string[] = ['Alice']
+    tokens.push(longToken)
+    tokens.push(shortToken)
+    //console.log(await shortToken.symbol(), await longToken.symbol(), longToken.address == shortToken.address)
+    //await generateReport(contractNames, contracts, tokens, addresses, addressNamesArray)
+
+    // approve base tokens to be pulled from caller
+    await baseToken.approve(house.address, ethers.constants.MaxUint256)
+    // approve base tokens to be pulled from caller
+    await quoteToken.approve(house.address, ethers.constants.MaxUint256)
+  })
+
+  afterEach(async function () {
+    let contractNames: string[] = ['House', 'Venue']
+    let contracts = [house, venue]
+    let addresses = [signer.address, MultiToken.address]
+    let addressNamesArray: string[] = ['Alice', 'MultiToken']
+    await generateReport(contractNames, contracts, tokens, addresses, addressNamesArray)
+  })
+
+  it('weth()', async () => {
+    expect(await venue.getWeth()).to.eq(weth.address)
+  })
+
+  it('house()', async () => {
+    expect(await venue.getHouse()).to.eq(house.address)
+  })
+
+  describe('Deposit', async () => {
+
+    it('Caller can use the house to deposit an option in a venue', async () => {
+      let params: any = venue.interface.encodeFunctionData('deposit', [oid, parseEther('1'), Alice])
+      await expect(house.execute(0, venue.address, params)).to.emit(house, 'Executed').withArgs(Alice, venue.address)
+    })
+
+    it('Caller can use the house to deposit an option in a venue on behalf of another contract or account', async () => {
+      let params: any = venue.interface.encodeFunctionData('deposit', [oid, parseEther('1'), house.address])
+      await expect(house.execute(0, venue.address, params)).to.emit(house, 'Executed').withArgs(Alice, venue.address)
+    })
+
+    it('Caller CANNOT use the house to deposit a non-existent option in a venue', async () => {
+      let params: any = venue.interface.encodeFunctionData('deposit', [false_oid, parseEther('1'), Alice])
+      await expect(house.execute(0, venue.address, params)).to.be.revertedWith("EXECUTION_FAIL")
+    })
+
+    it('Caller CANNOT use the house to deposit an option in a venue if they do not have a sufficient balance', async () => {
+      let params: any = venue.interface.encodeFunctionData('deposit', [oid, parseEther('10000000000000000'), Alice])
+      await expect(house.execute(0, venue.address, params)).to.be.revertedWith("EXECUTION_FAIL")
+    })
+
+  })
+
+  describe('Withdraw', async () => {
+
+    it('Caller can use the house to withdraw an option from a venue', async () => {
+      await venueDeposit(venue, Alice, oid)
+      let params: any = venue.interface.encodeFunctionData('withdraw', [oid, parseEther('1'), [Alice, Alice]])
+      await expect(house.execute(0, venue.address, params)).to.emit(house, 'Executed').withArgs(Alice, venue.address)
+    })
+
+    it('Caller CANNOT use the house to withdraw an option from a venue on behalf of another account', async () => {
+      await venueDeposit(venue, Alice, oid)
+      let params: any = venue.interface.encodeFunctionData('withdraw', [oid, parseEther('1'), [Alice, Alice]])
+      await expect(house.connect(signers[2]).execute(1, venue.address, params)).to.be.revertedWith("House: NOT_DEPOSITOR")
+    })
+
+    it('Caller CANNOT use the house to withdraw an option from a venue if they have no options deposited', async () => {
+      let params: any = venue.interface.encodeFunctionData('withdraw', [oid, parseEther('1'), [Alice, Alice]])
+      await expect(house.execute(0, venue.address, params)).to.be.revertedWith("EXECUTION_FAIL")
+    })
+
+  })
+
+  it('Caller can use a valid venue to borrow options without collateral', async () => {
+    let params: any = venue.interface.encodeFunctionData('borrowOptionTest', [oid, parseEther('1')])
+    await expect(house.execute(0, venue.address, params)).to.emit(house, 'Executed').withArgs(Alice, venue.address)
+  })
+
+  it('Depositor can use venue to mint long and short options, wrap them, and deposit them into the house', async () => {
+    let params: any = venue.interface.encodeFunctionData('mintOptionsThenWrap', [oid, parseEther('1'), Alice])
+    await expect(house.execute(0, venue.address, params)).to.emit(house, 'Executed').withArgs(Alice, venue.address)
+  })
+
+  it('Depositor can use venue to split wrapped options into long and short tokens and send them (update the balance of) the receivers.', async () => {
+    let receivers: string[] = [Alice, Alice]
+    await venueDeposit(venue, Alice, oid)
+    await venueMintWrap(venue, Alice, oid)
+    let params: any = venue.interface.encodeFunctionData('splitOptionsAndDeposit', [oid, parseEther('1'), receivers])
+    await expect(house.execute(1, venue.address, params)).to.emit(house, 'Executed').withArgs(Alice, venue.address)
+  })
+
+  it('Caller can use venue to exercise options (burn long tokens, pay quote tokens, and receive underlying)', async () => {
+    await venueDeposit(venue, Alice, oid)
+    await venueMintWrap(venue, Alice, oid)
+    await venueSplitDeposit(venue, Alice, oid)
+
+    let amount: BigNumber = parseEther('2')
+    // approve quote tokens to be pulled from user to the venue.
+    await quoteToken.approve(venue.address, ethers.constants.MaxUint256)
+    // deposit quote tokens to balance
+    let depositParams: any = venue.interface.encodeFunctionData('depositToken', [
+      quoteToken.address,
+      amount.mul(strikePrice).div(parseEther('1')),
+      Alice,
+    ])
+    await expect(house.execute(1, venue.address, depositParams))
+      .to.emit(house, 'Executed')
+      .withArgs(signer.address, venue.address)
+
+    let prevBaseBal = await baseToken.balanceOf(Alice)
+    let receiver: string = Alice
+    let params: any = venue.interface.encodeFunctionData('exerciseFromBalance', [oid, amount, receiver])
+    await expect(house.execute(1, venue.address, params)).to.emit(house, 'Executed').withArgs(signer.address, venue.address)
+    let postBaseBal = await baseToken.balanceOf(Alice)
+    let baseBalDiff = ethers.BigNumber.from(postBaseBal).sub(prevBaseBal)
+    expect(baseBalDiff).to.eq(amount)
+  })
+
+  it('Caller can use venue to redeem option and claim underlying when they hold both long and short tokens', async () => {
+    await venueDeposit(venue, Alice, oid)
+    await venueMintWrap(venue, Alice, oid)
+    await venueSplitDeposit(venue, Alice, oid)
+    // approve quote tokens to be pulled from user to the venue.
+    await quoteToken.approve(venue.address, ethers.constants.MaxUint256)
+    await quoteTokenDeposit(venue, Alice, quoteToken)
+    await venueExercise(venue, Alice, oid, quoteToken)
+    let amount: BigNumber = parseEther('1')
+
+    let prevBaseBal = await quoteToken.balanceOf(Alice)
+    let receiver: string = Alice
+    let params: any = venue.interface.encodeFunctionData('redeemFromBalance', [oid, amount, receiver])
+    await expect(house.execute(1, venue.address, params)).to.emit(house, 'Executed').withArgs(signer.address, venue.address)
+    let postBaseBal = await quoteToken.balanceOf(Alice)
+    let baseBalDiff = ethers.BigNumber.from(postBaseBal).sub(prevBaseBal)
+    expect(baseBalDiff).to.eq(amount.mul(strikePrice).div(parseEther('1')))
+  })
+
+  it('Caller can use venue to close options when they hold wrapped option tokens (which contain both long and short tokens)', async () => {
+    // mint and wrap 1 option
+    let amount: BigNumber = parseEther('2')
+    let receiver: string = Alice
+
+    let mintParams2: any = venue.interface.encodeFunctionData('deposit', [oid, amount, receiver])
+    await house.execute(0, venue.address, mintParams2)
+
+    let mintParams: any = venue.interface.encodeFunctionData('mintOptionsThenWrap', [oid, amount, receiver])
+    await expect(house.execute(0, venue.address, mintParams))
+      .to.emit(house, 'Executed')
+      .withArgs(signer.address, venue.address)
+
+    // redeem from wrapped balance
+    let prevBaseBal = await baseToken.balanceOf(Alice)
+    let params: any = venue.interface.encodeFunctionData('closeFromWrappedBalance', [oid, amount, receiver])
+    await expect(house.execute(1, venue.address, params)).to.emit(house, 'Executed').withArgs(signer.address, venue.address)
+    let postBaseBal = await baseToken.balanceOf(Alice)
+    let baseBalDiff = ethers.BigNumber.from(postBaseBal).sub(prevBaseBal)
+    expect(baseBalDiff).to.eq(amount)
+  })
+
+  it('Caller can use venue to close options when they hold both long and short tokens in the venue', async () => {
+    // approve tokens
+    const max = ethers.constants.MaxUint256
+    await longToken.approve(house.address, max)
+    await shortToken.approve(house.address, max)
+    // mint options and deposit
+    let amount: BigNumber = parseEther('1')
+    let receiver: string = Alice
+    let mintParams: any = venue.interface.encodeFunctionData('deposit', [oid, amount, receiver])
+    await expect(house.execute(0, venue.address, mintParams))
+      .to.emit(house, 'Executed')
+      .withArgs(signer.address, venue.address)
+
+    // close from balance
+    let prevBaseBal = await baseToken.balanceOf(Alice)
+    let params: any = venue.interface.encodeFunctionData('closeFromBalance', [oid, amount, receiver])
+    await expect(house.execute(0, venue.address, params)).to.emit(house, 'Executed').withArgs(signer.address, venue.address)
+    let postBaseBal = await baseToken.balanceOf(Alice)
+    let baseBalDiff = ethers.BigNumber.from(postBaseBal).sub(prevBaseBal)
+    expect(baseBalDiff).to.eq(amount)
+  })
+
+  it('Caller can use venue to close options when they hold both long and short options in their wallet', async () => {
+    // approve tokens
+    const max = ethers.constants.MaxUint256
+    await longToken.approve(house.address, max)
+    await shortToken.approve(house.address, max)
+    // mint options and deposit
+    let amount: BigNumber = parseEther('2')
+    let receiver: string = Alice
+    await venueDeposit(venue, Alice, oid)
+    await venueMintWrap(venue, Alice, oid)
+    await venueSplitDeposit(venue, Alice, oid)
+    let mintParams: any = venue.interface.encodeFunctionData('deposit', [oid, amount, receiver])
+    await expect(house.execute(1, venue.address, mintParams))
+      .to.emit(house, 'Executed')
+      .withArgs(signer.address, venue.address)
+
+    // withdraw options
+    let withdrawParams: any = venue.interface.encodeFunctionData('withdraw', [oid, amount, [receiver, receiver]])
+    await expect(house.execute(1, venue.address, withdrawParams))
+      .to.emit(house, 'Executed')
+      .withArgs(signer.address, venue.address)
+
+    let longBal = await longToken.balanceOf(Alice)
+    let shortBal = await shortToken.balanceOf(Alice)
+    expect(longBal).to.eq(shortBal).to.eq(amount)
+
+    // close from wallet
+    let prevBaseBal = await baseToken.balanceOf(Alice)
+    let params: any = venue.interface.encodeFunctionData('close', [oid, amount, receiver])
+    await expect(house.execute(1, venue.address, params)).to.emit(house, 'Executed').withArgs(Alice, venue.address)
+    let postBaseBal = await baseToken.balanceOf(Alice)
+    let baseBalDiff = ethers.BigNumber.from(postBaseBal).sub(prevBaseBal)
+    expect(baseBalDiff).to.eq(amount)
+  })
+})
